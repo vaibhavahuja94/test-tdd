18319a073b6d79e863f08f0f3aa08f6b46830335
Vaibhav Ahuja <sahujasmart@gmail.com>
2025-03-18 01:10:25 +0530

    handle nw lines between numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -4,12 +4,13 @@
  * If the string contains a single number, returns that number.
  * If multiple numbers are provided, returns their sum.
  * @param {string} numbers - A string of numbers.
- * @returns {number} Return a single number as is.
+ * @returns {number} Handle new lines (\n) between numbers.
  */
 function add(numbers) {
     if (numbers === "") return 0;
-    if (!numbers.includes(",")) return parseInt(numbers, 10);
-    const numArray = numbers.split(",").map(Number);
+    if (!numbers.includes(",") && !numbers.includes("\n")) return parseInt(numbers, 10);
+
+    const numArray = numbers.split(/,|\n/).map(Number);
     let sum = 0;
 
     for (let i = 0; i < numArray.length; i++) {
